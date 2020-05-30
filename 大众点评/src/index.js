597cf5d83d5284632d1db06939f324baff616032
Email <947201766@qq.com>
2020-05-30 20:22:22 +0800

    react-redux

--- 大众点评/src/index.js
+++ 大众点评/src/index.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunkMiddleware from "redux-thunk";
+import rootReducer from "./components-todo-redux/reducers";
 
 // 最简单的todo应用
-import App from './components-todo/App'; 
-ReactDOM.render(<App />, document.getElementById('root'));
+// import App from './components-todo/App'; 
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+import App from "./components-todo-redux/components/App";
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
