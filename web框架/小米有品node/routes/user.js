592d3853f2ae04fe840c3b8c50c97a69e85a31c6
Email <947201766@qq.com>
2020-08-08 17:12:27 +0800

    类

--- web框架/小米有品node/routes/user.js
+++ web框架/小米有品node/routes/user.js
@@ -10,8 +10,13 @@ const {
 const express = require('express'),
   route = express.Router();
 
-  //=>校验验证码是否正确
+//=>校验验证码是否正确
 route.get('/login', (req, res) => {
+  res.send(success(true, {
+    data: req.$USER_DATA
+  }));
+});
+route.get('/text', (req, res) => {
   res.send(success(true, {
     data: req.$USER_DATA
   }));
