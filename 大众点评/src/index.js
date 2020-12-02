14d72b98dda56d2925f655571a72b3ff8d87af88
Email <947201766@qq.com>
2020-12-02 22:44:42 +0800

    koa

--- 大众点评/src/index.js
+++ 大众点评/src/index.js
@@ -6,18 +6,18 @@ import thunkMiddleware from "redux-thunk";
 import rootReducer from "./components-todo-redux/reducers";
 
 // 最简单的todo应用
-// import App from './components-todo/App'; 
-// ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components-todo/App'; 
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // redux的todo应用
-import App from "./components-todo-redux/components/App";
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+// import App from "./components-todo-redux/components/App";
+// const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+// ReactDOM.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById("root")
+// );
 
 // react-router使用
 // import App from "./components-react-router/App";
