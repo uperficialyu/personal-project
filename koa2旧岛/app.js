f6c95de33648b754f6b874f1877b51ad6b0f73a3
Email <947201766@qq.com>
2020-12-05 09:03:09 +0800

    koa中间件

--- koa2旧岛/app.js
+++ koa2旧岛/app.js
@@ -1,1 +1,20 @@
 const koa = require('koa');
+
+const app = new koa();
+
+function test() {
+  console.log('hello world');
+}
+
+// 注册
+app.use((ctx, next) =>{
+  // 上下文
+  console.log('hello world');
+  next();
+})
+
+app.use(() => {
+  console.log('dsdds')
+})
+
+app.listen(3000);
