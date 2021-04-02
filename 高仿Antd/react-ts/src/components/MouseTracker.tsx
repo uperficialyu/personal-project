2c07ff6a1c0d06be1f30ca134c02e7314055fbc9
Email <947201766@qq.com>
2021-04-02 22:25:15 +0800

    鼠标

---
+++ 高仿Antd/react-ts/src/components/MouseTracker.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const MouseTracker: React.FC = () => {
+  const [ positions, setPositions ] = useState({x: 0, y: 0})
+  useEffect(() => {
+    const updateMouse= (e: MouseEvent) => {
+      setPositions({ x: e.clientX, y: e.clientY })
+    }
+    document.addEventListener('click', updateMouse)
+    return () => {
+      document.removeEventListener('click', updateMouse)
+    }
+  }, [])
+  return (
+    <p>X: {positions.x}, Y : {positions.y}</p>
+  )
+}
+
+export default MouseTracker;
