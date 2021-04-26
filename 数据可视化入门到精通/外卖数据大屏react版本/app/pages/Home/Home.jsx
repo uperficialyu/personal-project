f1cb0369f2875bc8113ca2f2504211855135d2ce
Email <947201766@qq.com>
2021-04-26 08:16:56 +0800

    代码提交

--- 数据可视化入门到精通/外卖数据大屏react版本/app/pages/Home/Home.jsx
+++ 数据可视化入门到精通/外卖数据大屏react版本/app/pages/Home/Home.jsx
@@ -3,14 +3,14 @@
  * @Author: yushunping
  * @Date: 2020-08-21 18:47:17
  * @Last Modified by:   yushunping
- * @Last Modified time: 2021-04-25 07:36:06
+ * @Last Modified time: 2021-04-25 19:48:36
  */
 
 import React, { Component, Fragment } from 'react';
 import Ajax from 'lib/ajax';
 import { sessionSet, sessionGet } from 'storage';
 import { FormattedMessage, injectIntl } from 'react-intl';
-// import Container from 'components/Container/Container';
+import Container from 'components/Container/Container';
 import TopHeader from 'components/TopHeader/TopHeader';
 import { inject, observer } from 'mobx-react';
 import { Button } from 'antd';
@@ -30,15 +30,15 @@ class Home extends Component {
     } = this.state;
     return (
       <React.Fragment>
-        <TopHeader />
-        {/* <Container>
+        <Container options={{width: 3840, height: 2160}}>
+          <TopHeader />
           <Button type="primary">Primary Button</Button>
           <Button>Default Button</Button>
           <Button type="dashed">Dashed Button</Button>
           <br />
           <Button type="text">Text Button</Button>
           <Button type="link">Link Button</Button>
-        </Container> */}
+        </Container>
       </React.Fragment>
     );
   }
